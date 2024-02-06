107dde5378b86ccf91e7364ece88306893f104b6
hori-hayato <152762969+hori-hayato@users.noreply.github.com>
2024-02-06 17:48:02 +0900

    kadai_020課題提出

---
+++ kadai_020/DictionaryExec_Chapter21.java
@@ -0,0 +1,13 @@
+package kadai_020;
+
+public class DictionaryExec_Chapter21 {
+	
+	public static void main(String[] agrs) {
+		var dict = new Dictionary_Chapter20();
+		var words = new String[] {"apple", "banana", "grape", "orange"};
+		dict.checkUp(words);
+		
+		}
+	}
+
+
